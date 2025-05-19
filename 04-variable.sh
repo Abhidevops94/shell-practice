17f24271b5c46bd06aca50d29dc85441e2e67b9f
Abhi <abhidevops94@gmail.com>
2025-05-20 00:07:00 +0530

    hello world begin

---
+++ 04-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1 #there should be no space between before or after =
+PERSON2=$2
+
+
+echo "$PERSON1:: Hey $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, i am fine. how are you doing?"
+echo "$PERSON1:: Iam fine too. What's up?"
+echo "$PERSON2:: nothing much, just going to Mars, will you come?"
+echo "$PERSON1:: sorry, you carrion."
