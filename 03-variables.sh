ea7d916c7446ad797a62ae20fb74c3847f2c99d0
Abhi <abhidevops94@gmail.com>
2025-05-20 00:03:07 +0530

    hello world begin

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PERSON1=Modi
+PERSON1=Modi #there should be no space between before or after =
 PERSON2=Musk
 
 
-echo "$PERSON1: Hey $PERSON2, how are you?"
-echo "$PERSON2: Hello $PERSON1, i am fine. how are you doing?"
-echo "$PERSON1: Iam fine too. What's up?"
+echo "$PERSON1:: Hey $PERSON2, how are you?"
+echo "$PERSON2:: Hello $PERSON1, i am fine. how are you doing?"
+echo "$PERSON1:: Iam fine too. What's up?"
 echo "$PERSON2:: nothing much, just going to Mars, will you come?"
 echo "$PERSON1:: sorry, you carrion."
