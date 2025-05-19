c3d9b6eb2729a1db55d7b51b337dfef0fc46c0c4
Abhi <abhidevops94@gmail.com>
2025-05-19 23:49:56 +0530

    hello world begin

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,5 +3,5 @@
 echo "Trump: Hey Musk, how are you?"
 echo "Musk: Hello Trump, i am fine. how are you doing?"
 echo "Trump: Iam fine too. What's up?"
-echo "Musk:: nothing muuch, just going to Mars, will you come?"
-echo "Musk:: sorry, you carrion.
+echo "Musk:: nothing much, just going to Mars, will you come?"
+echo "Musk:: sorry, you carrion."
