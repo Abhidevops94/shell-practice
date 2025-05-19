3ac75977901927735a7a28f4dd40018f7a3285b5
Abhi <abhidevops94@gmail.com>
2025-05-20 00:50:22 +0530

    hello world begin

--- 07-array.sh
+++ 07-array.sh
@@ -3,7 +3,7 @@
 MOVIES=("court" "pushpa2" "hit3" "thandel" )
 
 echo "first movie is: ${MOVIES[0]}"
-echo "first movie is: ${MOVIES[3]}"
+echo "last movie is: ${MOVIES[3]}"
 echo "first movie is: ${MOVIES[4]}"
 
 echo "All movies: ${MOVIES[@]}"
