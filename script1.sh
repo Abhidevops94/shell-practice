e288ccab84abe37dde0821996d9625eabb02e65c
Abhi <abhidevops94@gmail.com>
2025-05-31 22:00:38 +0530

    Hello world started

---
+++ script1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GREETINGS="HELLO, Good Morning"
+echo "$GREETING"
+echo "PID of SCRIPT1: $$"
+
+./script2.sh
---
+++ script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "PID of SCRIPT1: $$"
+echo "$GREETING"
