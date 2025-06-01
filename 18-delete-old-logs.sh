d484587a6ecbf1a81ac431557c9971073f0cc8cc
Abhi <abhidevops94@gmail.com>
2025-06-01 18:26:48 +0530

    shellscript

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+SOURCE_DIR=/home/ec2-user/app-logs
+
+mkdir -p $LOGS_FOLDER
+echo "script started excuting at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are running with root access" | tee -a $LOG_FILE
+fi
+
+#Validate function takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo -e "$2 is ... $G success $N" | tee -a $LOG_FILE
+else
+    echo -e "$2 is ... $R failure $N" | tee -a $LOG_FILE
+    exit 1
+fi
+}
+
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+while IFS= read -r filepath
+do
+    echo "deleting file: $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
+done <<< $FILE_TO_DELETE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "script is completed successfully, $Y time taken: $TOTAL_TIME $N" | tee -a $LOG_FILE
